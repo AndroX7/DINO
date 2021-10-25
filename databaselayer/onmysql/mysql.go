d90becb8c9ead855bc12fa1b57d9f4c7a26480cd
AndroX7 <claudiovivaldi389@gmail.com>
2021-10-25 18:47:48 +0700

    mysql layer

--- databaselayer/onmysql/mysql.go
+++ databaselayer/onmysql/mysql.go
@@ -18,15 +18,16 @@ type animal struct {
 
 func main() {
 
+	password := "aisgamming123#"
 	// connect to database
-	db, err := sql.Open("mysql", "root:password@/dino")
+	db, err := sql.Open("mysql", fmt.Sprintf("root:%s@/dino", password))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
 	// general query with arguments
-	rows, err := db.Query("select * from Dino.animals where age > ?", 10)
+	rows, err := db.Query("select * from dino.animals where age > ?", 10)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -50,7 +51,8 @@ func main() {
 
 	fmt.Println(animals)
 
-	row := db.QueryRow("select * from Dino.animals where id = ?", 2)
+	// query single row
+	row := db.QueryRow("select * from dino.animals where id = ?", 3)
 	a := animal{}
 	err = row.Scan(&a.id, &a.animalType, &a.nickname, &a.zone, &a.age)
 	if err != nil {
@@ -58,4 +60,23 @@ func main() {
 	}
 
 	fmt.Println(a)
+
+	// // insert row
+	// result, err := db.Exec("Insert into dino.animals (animal_type,nickname,zone,age) values ('Carnotosaurus', 'Carno2', ?, ?)", 3, 22)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(result.LastInsertId())
+	// fmt.Println(result.RowsAffected())
+
+	//update fields which id is 3
+	age := 16
+	id := 3
+	result, err := db.Exec("update dino.animals set age = ? where id = ?", age, id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(result.LastInsertId())
+	fmt.Println(result.RowsAffected())
+
 }
